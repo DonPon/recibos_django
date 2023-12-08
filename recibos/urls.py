71ca7f9abafe4bea99b561157b6f680c2d080fcd
Franzeck <99328872+DonPon@users.noreply.github.com>
2023-12-08 14:46:54 +0100

    Add files via upload

--- recibos/urls.py
+++ recibos/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     # ... your other URL patterns
     path('', generate_pdfs, name='generate_pdfs'),
     path('update_tenants/', update_tenants, name='update_tenants'),
-    path('update_tenants/<str:tenant_name>/', update_tenants, name='update_tenant'),
+    path('update_tenants/<tenant_name>/', update_tenants, name='update_tenant'),
     path('update_success/', update_success, name='update_success'),
 ]
