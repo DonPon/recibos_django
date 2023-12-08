4e8a97596628ff0b584c6925d8d556f698fd2976
Franzeck <99328872+DonPon@users.noreply.github.com>
2023-12-08 16:49:25 +0100

    Add files via upload

--- recibos_django/settings.py
+++ recibos_django/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'tmp')
-MEDIA_URL = 'tmp/'
+MEDIA_URL = '/tmp/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
