66818d1873c1937f6a1bcb38c5245fc889e8bd3e
Franzeck <99328872+DonPon@users.noreply.github.com>
2023-12-08 21:41:50 +0100

    Update settings.py

--- recibos_django/settings.py
+++ recibos_django/settings.py
@@ -127,8 +127,8 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'tmp')
-MEDIA_ROOT = '/opt/render/project/src/tmp/'
-MEDIA_URL = '/tmp/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
