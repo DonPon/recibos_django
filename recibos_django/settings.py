56ef4351c9962af8cc9105bf173acf3daa7ee721
DonPon <franzeckermann@gmail.com>
2024-09-20 14:29:19 +0200

    improved session security

--- recibos_django/settings.py
+++ recibos_django/settings.py
@@ -159,3 +159,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SESSION_COOKIE_AGE = 1800
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
